afa48e28d7009781c253f5a91a1a0b807d0c0429
Art Guerrero <artemio.guerreru@g.austincc.edu>
2025-03-24 13:28:46 -0500

    create scanner object for user input,create int variable for captureing the length of the array,create boolean for checkingif user input is valid or not,while user input isinvalid do this,if object is a integer do this,set checker to true,save user input as the length of list,create array variable,for loop that runs the length of the number the user input,every time this itterates it will ask the user for a number

--- App.java
+++ App.java
@@ -1,5 +1,38 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // create scanner object for user input
+        Scanner sc = new Scanner(System.in);
+        // create int variable for captureing the length of the array
+        int lengthList = 0;
+        // create boolean for checkingif user input is valid or not
+        boolean validInput = false;
+
+        // while user input isinvalid do this
+        while (!validInput) {
+            System.out.println("How many inputs will the length of this list be: ");
+            // if object is a integer do this
+            if (sc.hasNextInt()) {
+                // set checker to true
+                validInput = true;
+                // save user input as the length of list
+                lengthList = sc.nextInt();
+                // create array variable
+                int[] inArray = new int[lengthList];
+                // for loop that runs the length of the number the user input
+                for (int i = 0; i <= lengthList - 1; i++) { 
+                    // every time this itterates it will ask the user for a number
+                    System.out.println("What is the " + (i + 1) + " number in the list: ");
+                    inArray[i] = sc.nextInt();
+                }
+                System.out.println("You entered: " + Arrays.toString(inArray));
+            }
+            
+            else{
+                System.out.println("Invalid input: ");
+                sc.next();
+            }
+        }
     }
 }
